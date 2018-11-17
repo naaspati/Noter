a63375ed616dd1f89a5f621cac0f21eecc640b76
naaspati <naaspati@gmail.com>
2018-11-17 10:07:28 +0530

    minor

--- src/main/java/sam/extra/EnvKeys.java
+++ src/main/java/sam/extra/EnvKeys.java
@@ -7,20 +7,21 @@ import java.util.Optional;
 import sam.myutils.MyUtilsException;
 import sam.myutils.System2;
 
-public class EnvKeys {
+public interface EnvKeys {
 	
 	/**
 	 * working dir for app
 	 */
-	public static final Path APP_DATA = Optional.ofNullable(System2.lookupAny("app_data", "APP_DATA","app.data", "APP.DATA"))
+	final Path APP_DATA = Optional.ofNullable(System2.lookupAny("app_data", "APP_DATA","app.data", "APP.DATA"))
 			.map(Paths::get)
 			.orElseGet(() -> MyUtilsException.noError(() -> Paths.get(ClassLoader.getSystemResource(".").toURI()).resolve("app_data")));;
 	/**
 	 * dir to watch for files with commands to open files
 	 */
-	public static final boolean OPEN_CMD_ENABLE = "false".equals(Optional.ofNullable(System2.lookup("noter.open.cmd.enable")).map(s -> s.trim().toLowerCase()).orElse(null)) ? false : true;
-	public static final Path OPEN_CMD_DIR = APP_DATA.resolve("cmd_open");
-	public static final String DYNAMIC_MENUS_FILE = "dynamic.menus.file";
-	public static final String PLUGIN_DIR = "plugins_dir";
-	public static final String DEFAULT_SAVE_DIR = "default.save.dir";
+	final boolean OPEN_CMD_ENABLE = System2.lookupBoolean("noter.open.cmd.enable", true);
+	final Path OPEN_CMD_DIR = APP_DATA.resolve("cmd_open");
+	final String DYNAMIC_MENUS_FILE = "dynamic.menus.file";
+	final String PLUGIN_DIR = "plugins_dir";
+	final String DEFAULT_SAVE_DIR = "default.save.dir";
+	final boolean ENABLE_FILE_LOOKUP_OPEN_CACHE = System2.lookupBoolean("file.lookup.open.cache.enable", false);
 }
--- src/main/java/sam/noter/FilesLookup.java
+++ src/main/java/sam/noter/FilesLookup.java
@@ -1,6 +1,6 @@
 package sam.noter;
 
-import static sam.extra.EnvKeys.DEFAULT_SAVE_DIR;
+import static sam.extra.EnvKeys.*;
 import static sam.noter.Utils.APP_DATA;
 
 import java.io.File;
@@ -24,6 +24,7 @@ import sam.extra.EnvKeys;
 import sam.io.serilizers.StringReader2;
 import sam.io.serilizers.StringWriter2;
 import sam.logging.MyLoggerFactory;
+import sam.myutils.MyUtilsCheck;
 import sam.myutils.System2;
 
 public class FilesLookup {
@@ -32,18 +33,18 @@ public class FilesLookup {
 	public List<Path> parse(List<String> args) throws IOException {
 		if(args.isEmpty()) 
 			return Collections.emptyList();
-		
+
 		if(args.size() == 1) {
 			File p = find(args.get(0));
 			if(p != null) 
 				return new ArrayList<>(Arrays.asList(p.toPath()));
 		}
-		
+
 		List<Path> files = new ArrayList<>();
-		
+
 		for (String s : args) {
 			if(s.trim().isEmpty()) continue;
-			
+
 			File f = find(s);
 			if(f == null)
 				LOGGER.severe("file not found for: "+s);
@@ -56,34 +57,22 @@ public class FilesLookup {
 	private Map<String, Path> files;
 	private String[] opencache ;
 	private final Path openCacheDir = APP_DATA.resolve("open_cache");
-	
+
 	private File find(final String string) throws UnsupportedEncodingException, IOException {
-		File f = null;
-		
-		if(opencache == null) {
-			opencache = openCacheDir.toFile().list();
-			if(opencache == null) 
-				opencache = new String[0];
-			Arrays.sort(opencache, Comparator.naturalOrder());
-		}
-		if(Arrays.binarySearch(opencache, string) >= 0) {
-			f = new File(StringReader2.getText(openCacheDir.resolve( string)));
-			if(f.exists()) {
-				File f2 = f;
-				LOGGER.info(() -> "loaded from open_cache: "+f2);
-				return f;
-			}
-		}
-		
+		File f = ENABLE_FILE_LOOKUP_OPEN_CACHE ? openCacheLookup(string) : null;
+
+		if(f != null) return f;
+
 		f = new File(string);
 		if(f.exists()) {
-			LOGGER.info(() -> "open as file found: "+string);
+			LOGGER.info(() -> "FILE_FOUND: "+string);
 			return f;
 		};
-		
+
+
 		if(files == null) {
 			Path dd = defaultDir();
-			if(Files.notExists(dd)) {
+			if(MyUtilsCheck.notExists(dd)) {
 				files = Collections.emptyMap();
 				LOGGER.severe("default dir, not found: "+dd);
 			} else {
@@ -94,40 +83,61 @@ public class FilesLookup {
 					if(!s.endsWith(".jbook"))
 						return;
 					if(!Files.isRegularFile(p)) return;
-					
+
 					files.put(substring(s), p);
 				});
 			}			
 		}
-		
+
 		if(files.isEmpty()) return null;
-		
+
 		String s = string.toLowerCase(); 
 		Path p = files.get(s.endsWith(".jbook") ? substring(s) : s);
 		if(p == null) 
 			p = files.entrySet().stream().filter(e -> e.getKey().startsWith(string)).findFirst().map(e -> e.getValue()).orElse(null);
-		
+
 		if(p == null) return null;
 		Path p2 = p;
-		LOGGER.info(() -> "find from defaultDir walk, "+string+", path: "+p2);
+		LOGGER.info(() -> "DEFAULT_DIR_WALK: "+string+"= "+p2);
 		save(string, p);
 		return p.toFile();
+	}
+
+	private File openCacheLookup(String string) throws IOException {
+		File f = null;
+
+		if(opencache == null) {
+			opencache = openCacheDir.toFile().list();
+			if(opencache == null) 
+				opencache = new String[0];
+			Arrays.sort(opencache, Comparator.naturalOrder());
+		}
+		if(Arrays.binarySearch(opencache, string) >= 0) {
+			f = new File(StringReader2.getText(openCacheDir.resolve( string)));
+			if(f.exists()) {
+				File f2 = f;
+				LOGGER.info(() -> "OPEN_CACHE: "+string+"="+f2);
+				return f;
+			}
+		}
+		return null;
 	}
 
 	private String substring(String s) {
 		return s.substring(0, s.length() - 6);
 	}
 
-	private Path dd;
 	private Path defaultDir() {
-		if(dd != null) return dd;
 		String s = System2.lookup(DEFAULT_SAVE_DIR);
 		if(s == null)
-			s = Optional.ofNullable(SessionFactory.sharedSession().getProperty(EnvKeys.DEFAULT_SAVE_DIR)).orElse(System.getenv("USERPROFILE"));
-		return dd = Paths.get(s);
+			return Optional.ofNullable(SessionFactory.sharedSession().getProperty(EnvKeys.DEFAULT_SAVE_DIR)).map(Paths::get).orElse(null);
+		else 
+			return Paths.get(s);
 	}
 
 	private void save(String key, Path path) throws IOException {
+		if(!ENABLE_FILE_LOOKUP_OPEN_CACHE) return;
+
 		Path p = openCacheDir.resolve( key);
 		Files.createDirectories(p.getParent());
 		StringWriter2.setText(p, path.toString());
